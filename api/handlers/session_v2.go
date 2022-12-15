c1927cc85b2f29cc5241eaf499f60b30876cb93f
yuldashboev <team19devs@gmail.com>
2022-12-15 14:26:05 +0500

    [feat] /v2/login/superadmin

--- api/handlers/session_v2.go
+++ api/handlers/session_v2.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"errors"
 	"strings"
 	"ucode/ucode_go_auth_service/api/http"
+	"ucode/ucode_go_auth_service/pkg/helper"
 
 	"ucode/ucode_go_auth_service/genproto/auth_service"
+	"ucode/ucode_go_auth_service/genproto/object_builder_service"
 
 	"github.com/gin-gonic/gin"
 )
@@ -103,4 +106,86 @@ func (h *Handler) V2RefreshToken(c *gin.Context) {
 	}
 
 	h.handleResponse(c, http.OK, resp)
+}
+
+// V2LoginSuperAdmin godoc
+// @ID V2login
+// @Router /v2/login/superadmin [POST]
+// @Summary V2LoginSuperAdmin
+// @Description V2LoginSuperAdmin
+// @Tags V2_Session
+// @Accept json
+// @Produce json
+// @Param login body auth_service.V2LoginSuperAdminRequest true "LoginSuperAdminRequestBody"
+// @Success 201 {object} http.Response{data=string} "User data"
+// @Response 400 {object} http.Response{data=string} "Bad Request"
+// @Failure 500 {object} http.Response{data=string} "Server Error"
+func (h *Handler) V2LoginSuperAdmin(c *gin.Context) {
+	var login auth_service.V2LoginRequest
+	err := c.ShouldBindJSON(&login)
+	if err != nil {
+		h.handleResponse(c, http.BadRequest, err.Error())
+		return
+	}
+
+	userReq, err := helper.ConvertMapToStruct(map[string]interface{}{
+		"login":    login.Username,
+		"password": login.Password,
+	})
+	if err != nil {
+		h.handleResponse(c, http.BadRequest, err.Error())
+		return
+	}
+
+	userResp, err := h.services.ObjectBuilderService().GetSingle(
+		context.Background(),
+		&object_builder_service.CommonMessage{
+			TableSlug: "user",
+			Data:      userReq,
+		})
+	if err != nil {
+		h.handleResponse(c, http.BadRequest, err.Error())
+		return
+	}
+
+	userData := userResp.Data.AsMap()["data"].(map[string]interface{})
+	userClientTypeID, ok := userData["client_type_id"].(string)
+	if !ok {
+		h.handleResponse(c, http.BadRequest, "Необходимо выбрать тип пользователя")
+		return
+	}
+
+	login.ClientType = userClientTypeID
+
+	resp, err := h.services.SessionService().V2Login(
+		c.Request.Context(),
+		&login,
+	)
+	httpErrorStr := ""
+	if err != nil {
+		httpErrorStr = strings.Split(err.Error(), "=")[len(strings.Split(err.Error(), "="))-1][1:]
+		httpErrorStr = strings.ToLower(httpErrorStr)
+	}
+	if httpErrorStr == "user not found" {
+		err := errors.New("Пользователь не найдено")
+		h.handleResponse(c, http.NotFound, err.Error())
+		return
+	} else if httpErrorStr == "user has been expired" {
+		err := errors.New("Срок действия пользователя истек")
+		h.handleResponse(c, http.InvalidArgument, err.Error())
+		return
+	} else if httpErrorStr == "invalid username" {
+		err := errors.New("Неверное имя пользователя")
+		h.handleResponse(c, http.InvalidArgument, err.Error())
+		return
+	} else if httpErrorStr == "invalid password" {
+		err := errors.New("Неверное пароль")
+		h.handleResponse(c, http.InvalidArgument, err.Error())
+		return
+	} else if err != nil {
+		h.handleResponse(c, http.GRPCError, err.Error())
+		return
+	}
+
+	h.handleResponse(c, http.Created, resp)
 }
