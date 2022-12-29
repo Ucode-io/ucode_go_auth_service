50ea5644df342b35b11d759f9a37054467001eb8
euler <elyorbekuralov@gmail.com>
2022-12-29 20:05:24 +0500

    migration fixed

--- migrations/postgres/000015_alter_table_user.up.sql
+++ migrations/postgres/000015_alter_table_user.up.sql
@@ -1,8 +1,8 @@
 alter table "user"
-add constraint user_unq unique(login);
+add constraint user_unq_login unique(login);
 
 alter table "user"
-add constraint user_unq unique(email);
+add constraint user_unq_email unique(email);
 
 alter table "user"
-add constraint user_unq unique(phone);
+add constraint user_unq_phone unique(phone);
