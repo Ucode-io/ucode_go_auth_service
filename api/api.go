3d1e63fe008954230eb349c6303dd7e3b302f402
yuldashboev <team19devs@gmail.com>
2022-12-29 17:27:40 +0500

    fixed user invite api

--- api/api.go
+++ api/api.go
@@ -164,7 +164,7 @@ func SetUpRouter(h handlers.Handler, cfg config.Config) (r *gin.Engine) {
 		v2.PUT("/refresh-superadmin", h.V2RefreshTokenSuperAdmin)
 		v2.POST("/login/superadmin", h.V2LoginSuperAdmin)
 		v2.POST("/multi-company/login", h.V2MultiCompanyLogin)
-		v2.POST("/add-user-to-project", h.AddUserToProject)
+		v2.POST("/user/invite", h.AddUserToProject)
 	}
 
 	//COMPANY
