65537a72061a9035b06901947a102ca8b2974d5b
Sirojiddin-kh <sirojiddinhamodov2001@gmail.com>
2022-12-29 11:22:37 +0500

    user table quoted

--- migrations/postgres/000013_alter_table_user.up.sql
+++ migrations/postgres/000013_alter_table_user.up.sql
@@ -1,3 +1,3 @@
-ALTER TABLE auth_service.user DROP COLUMN  IF EXISTS project_id;
+ALTER TABLE "user" DROP COLUMN  IF EXISTS project_id;
 
-ALTER TABLE auth_service.user ADD COLUMN company_id uuid;
+ALTER TABLE "user" ADD COLUMN company_id uuid;
