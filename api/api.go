f0f6dfa555a59c1123fa90f9b947e2d5b7336bb5
euler <elyorbekuralov@gmail.com>
2022-12-29 19:29:45 +0500

    added update user data in mongo

--- api/api.go
+++ api/api.go
@@ -175,6 +175,7 @@ func SetUpRouter(h handlers.Handler, cfg config.Config) (r *gin.Engine) {
 	//PROJECT
 	r.POST("/project", h.CreateProject)
 	r.PUT("/project", h.UpdateProject)
+	r.PUT("/project/:project-id/user-update", h.UpdateProjectUserData)
 	r.GET("/project", h.GetProjectList)
 	r.GET("project/:project-id", h.GetProjectByID)
 	r.DELETE("/project/:project-id", h.DeleteProject)
