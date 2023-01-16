54bfc2410f91405376c1811491479b49528614a3
euler <elyorbekuralov@gmail.com>
2023-01-16 15:30:07 +0500

    add resource id

---
+++ api/handlers/middleware.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//var (
+		//	res *auth_service.V2HasAccessUserRes
+		//	ok  bool
+		//)
+		//host := c.Request.Host
+		//
+		//if strings.Contains(host, CLIENT_HOST) {
+		//	res, ok = h.hasAccess(c)
+		//	if !ok {
+		//		c.Abort()
+		//		return
+		//	}
+		//}
+
+		resourceId := c.GetHeader("Resource-Id")
+
+		//c.Set("Auth", res)
+		c.Set("resource_id", resourceId)
+		//c.Set("namespace", h.cfg.UcodeNamespace)
+		c.Next()
+	}
+}
