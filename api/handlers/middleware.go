eb131546d1eba430794ab2f77b068dd5a30a6607
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-02-15 19:21:06 +0500

    add api key to middleware

--- api/handlers/middleware.go
+++ api/handlers/middleware.go
@@ -1,6 +1,11 @@
 package handlers
 
 import (
+	"strings"
+	"ucode/ucode_go_auth_service/api/http"
+	"ucode/ucode_go_auth_service/genproto/auth_service"
+	"ucode/ucode_go_auth_service/genproto/company_service"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,9 +24,42 @@ func (h *Handler) AuthMiddleware() gin.HandlerFunc {
 		//		return
 		//	}
 		//}
-
 		resourceId := c.GetHeader("Resource-Id")
 		environmentId := c.GetHeader("Environment-Id")
+
+		bearerToken := c.GetHeader("Authorization")
+		if bearerToken != "" {
+			strArr := strings.Split(bearerToken, " ")
+	
+			if strArr[0] == "API-KEY" {
+				app_id := c.GetHeader("X-API-KEY")
+				apikeys, err := h.services.ApiKeysService().GetEnvID(
+					c.Request.Context(),
+					&auth_service.GetReq{
+						Id: app_id,
+					},
+				)
+				if err != nil {
+					h.handleResponse(c, http.BadRequest, err.Error())
+					c.Abort()
+					return
+				}
+	
+				resource, err := h.services.ResourceService().GetResourceByEnvID(
+					c.Request.Context(),
+					&company_service.GetResourceByEnvIDRequest{
+						EnvId: apikeys.GetEnvironmentId(),
+					},
+				)
+				if err != nil {
+					h.handleResponse(c, http.BadRequest, err.Error())
+					c.Abort()
+					return
+				}
+				resourceId = resource.GetResource().GetId()
+				environmentId = apikeys.GetEnvironmentId()
+			}
+		}
 
 		//c.Set("Auth", res)
 		c.Set("resource_id", resourceId)
