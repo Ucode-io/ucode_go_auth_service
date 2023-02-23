18b6713e35a488b8975fcde2072bec4fae52c4b0
dilmurodov <tolibdilmurodov98@gmail.com>
2023-02-23 18:09:41 +0500

    add x-api-key

--- pkg/helper/session_helpers.go
+++ pkg/helper/session_helpers.go
@@ -1,77 +1,81 @@
 package helper
 
 import (
+	"log"
 	pb "ucode/ucode_go_auth_service/genproto/auth_service"
 	pbObject "ucode/ucode_go_auth_service/genproto/object_builder_service"
 )
 
 func ConvertPbToAnotherPb(data *pbObject.V2LoginResponse) *pb.V2LoginResponse {
+
+	log.Printf("ConvertPbToAnotherPb: %v", data)
+
 	res := &pb.V2LoginResponse{}
-	res.UserId = data.UserId
-	res.LoginTableSlug = data.LoginTableSlug
-	tables := make([]*pb.Table, 0, len(data.ClientType.Tables))
-	for _, v := range data.ClientType.Tables {
+	res.UserId = data.GetUserId()
+	res.LoginTableSlug = data.GetLoginTableSlug()
+	tables := make([]*pb.Table, 0, len(data.GetClientType().GetTables()))
+	for _, v := range data.GetClientType().GetTables() {
 		table := &pb.Table{}
-		table.Data = v.Data
-		table.Icon = v.Icon
-		table.Label = v.Label
-		table.Slug = v.Slug
-		table.ViewLabel = v.ViewLabel
-		table.ViewSlug = v.ViewSlug
+		table.Data = v.GetData()
+		table.Icon = v.GetIcon()
+		table.Label = v.GetLabel()
+		table.Slug = v.GetSlug()
+		table.ViewLabel = v.GetViewLabel()
+		table.ViewSlug = v.GetViewSlug()
 		tables = append(tables, table)
 	}
 
 	res.ClientType = &pb.ClientType{
-		Id:           data.ClientType.Guid,
-		Name:         data.ClientType.Name,
-		ConfirmBy:    pb.ConfirmStrategies(data.ClientType.ConfirmBy),
-		SelfRegister: data.ClientType.SelfRegister,
-		SelfRecover:  data.ClientType.SelfRecover,
-		ProjectId:    data.ClientType.ProjectId,
+		Id:           data.GetClientType().GetGuid(),
+		Name:         data.GetClientType().GetName(),
+		ConfirmBy:    pb.ConfirmStrategies(data.GetClientType().GetConfirmBy()),
+		SelfRegister: data.GetClientType().GetSelfRegister(),
+		SelfRecover:  data.GetClientType().GetSelfRecover(),
+		ProjectId:    data.GetClientType().GetProjectId(),
 		Tables:       tables,
 	}
 
 	res.ClientPlatform = &pb.ClientPlatform{
-		Id:        data.ClientPlatform.GetGuid(),
-		Name:      data.ClientPlatform.GetName(),
-		ProjectId: data.ClientPlatform.GetProjectId(),
-		Subdomain: data.ClientPlatform.GetSubdomain(),
+		Id:        data.GetClientPlatform().GetGuid(),
+		Name:      data.GetClientPlatform().GetName(),
+		ProjectId: data.GetClientPlatform().GetProjectId(),
+		Subdomain: data.GetClientPlatform().GetSubdomain(),
 	}
-	permissions := make([]*pb.RecordPermission, 0, len(data.Permissions))
-	for _, v := range data.Permissions {
+	permissions := make([]*pb.RecordPermission, 0, len(data.GetPermissions()))
+	for _, v := range data.GetPermissions() {
 		permission := &pb.RecordPermission{}
-		permission.ClientTypeId = v.ClientTypeId
-		permission.Id = v.Guid
-		permission.Read = v.Read
-		permission.Write = v.Write
-		permission.Delete = v.Delete
-		permission.Update = v.Update
-		permission.RoleId = v.RoleId
-		permission.TableSlug = v.TableSlug
+		permission.ClientTypeId = v.GetClientTypeId()
+		permission.Id = v.GetGuid()
+		permission.Read = v.GetRead()
+		permission.Write = v.GetWrite()
+		permission.Delete = v.GetDelete()
+		permission.Update = v.GetUpdate()
+		permission.RoleId = v.GetRoleId()
+		permission.TableSlug = v.GetTableSlug()
 		permissions = append(permissions, permission)
 	}
 
-	appPermissions := make([]*pb.RecordPermission, 0, len(data.Permissions))
-	for _, v := range data.AppPermissions {
+	appPermissions := make([]*pb.RecordPermission, 0, len(data.GetPermissions()))
+	for _, v := range data.GetAppPermissions() {
 		appPermission := &pb.RecordPermission{}
-		appPermission.ClientTypeId = v.ClientTypeId
-		appPermission.Id = v.Guid
-		appPermission.Read = v.Read
-		appPermission.Write = v.Write
-		appPermission.Delete = v.Delete
-		appPermission.Update = v.Update
-		appPermission.RoleId = v.RoleId
-		appPermission.TableSlug = v.TableSlug
+		appPermission.ClientTypeId = v.GetClientTypeId()
+		appPermission.Id = v.GetGuid()
+		appPermission.Read = v.GetRead()
+		appPermission.Write = v.GetWrite()
+		appPermission.Delete = v.GetDelete()
+		appPermission.Update = v.GetUpdate()
+		appPermission.RoleId = v.GetRoleId()
+		appPermission.TableSlug = v.GetTableSlug()
 		appPermissions = append(appPermissions, appPermission)
 	}
 	res.Permissions = permissions
 	res.AppPermissions = appPermissions
 	res.Role = &pb.Role{
-		Id:               data.Role.Guid,
-		ClientTypeId:     data.Role.ClientTypeId,
-		Name:             data.Role.Name,
-		ClientPlatformId: data.Role.ClientPlatformId,
-		ProjectId:        data.Role.ProjectId,
+		Id:               data.GetRole().GetGuid(),
+		ClientTypeId:     data.GetRole().GetClientTypeId(),
+		Name:             data.GetRole().GetName(),
+		ClientPlatformId: data.GetRole().GetClientPlatformId(),
+		ProjectId:        data.GetRole().GetProjectId(),
 	}
 	return res
 }
