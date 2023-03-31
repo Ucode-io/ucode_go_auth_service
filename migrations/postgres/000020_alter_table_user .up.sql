911d6c55e31fd6a406752755feaf29b23e57a6fd
abdinazarov <abdinazarovgithub17@gmail.com>
2023-03-31 17:36:00 +0500

    fix: 20migration up

--- migrations/postgres/000020_alter_table_user .up.sql
+++ migrations/postgres/000020_alter_table_user .up.sql
@@ -2,21 +2,21 @@ ALTER TABLE "user" DROP CONSTRAINT IF EXISTS user_unq_login;
 
 ALTER TABLE "user" DROP CONSTRAINT IF EXISTS user_unq_phone;
 
-CREATE UNIQUE INDEX user_unq_login ON "user" (login)
+CREATE UNIQUE INDEX if not EXISTS user_unq_login ON "user" (login)
 WHERE(
    NOT (
      ( login IS NULL  OR  login = '' )
    )
 );
 
-CREATE UNIQUE INDEX user_unq_phone ON "user" (phone)
+CREATE UNIQUE INDEX if not EXISTS user_unq_phone ON "user" (phone)
 WHERE(
    NOT (
      ( phone IS NULL  OR  phone = '' )
    )
 );
 
-CREATE UNIQUE INDEX user_login_and_phone_and_email ON "user" (phone, login, email)
+CREATE UNIQUE INDEX if not EXISTS user_login_and_phone_and_email ON "user" (phone, login, email)
 WHERE (
    NOT (
      ( login IS NULL  OR  login = '' )
