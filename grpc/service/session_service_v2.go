6e8bfee690647020cfd32a49f559dfd2b763885e
dilmurodov <tolibdilmurodov98@gmail.com>
2023-04-17 10:04:16 +0500

    [*] fix with login data

--- grpc/service/session_service_v2.go
+++ grpc/service/session_service_v2.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"strings"
 	"time"
 	"ucode/ucode_go_auth_service/config"
@@ -70,16 +71,18 @@ func (s *sessionService) V2Login(ctx context.Context, req *pb.V2LoginRequest) (*
 	// 	s.log.Error("!!!Login--->", logger.Error(err))
 	// 	return nil, status.Error(codes.InvalidArgument, err.Error())
 	// }
-	fmt.Println("TEST::::6")
-	fmt.Println(req.ResourceEnvironmentId)
+
+	reqLoginData := &pbObject.LoginDataReq{
+		UserId:                user.GetId(),
+		ClientType:            req.ClientType,
+		ProjectId:             req.GetProjectId(),
+		ResourceEnvironmentId: req.GetResourceEnvironmentId(),
+	}
+	log.Println("reqLoginData--->", reqLoginData)
+
 	data, err := s.services.LoginService().LoginData(
 		ctx,
-		&pbObject.LoginDataReq{
-			UserId:                user.GetId(),
-			ClientType:            req.ClientType,
-			ProjectId:             req.GetProjectId(),
-			ResourceEnvironmentId: req.GetResourceEnvironmentId(),
-		},
+		reqLoginData,
 	)
 	if err != nil {
 		errGetUserProjectData := errors.New("invalid user project data")
@@ -130,7 +133,7 @@ func (s *sessionService) V2Login(ctx context.Context, req *pb.V2LoginRequest) (*
 	if req.Tables != nil {
 		res.Tables = req.Tables
 	}
-	
+
 	return res, nil
 }
 
