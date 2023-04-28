2dba235194452825ff4b95a60715a44cc36bf129
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-04-28 11:12:28 +0500

    add: regex phone

--- vendor/github.com/saidamir98/udevs_pkg/util/phone.go
+++ vendor/github.com/saidamir98/udevs_pkg/util/phone.go
@@ -8,5 +8,12 @@ import "regexp"
 // Note: This function does not check if the phone is already in use.
 func IsValidPhone(phone string) bool {
 	r := regexp.MustCompile(`^\+998[0-9]{2}[0-9]{7}$`)
-	return r.MatchString(phone)
+	s := regexp.MustCompile(`^\(?[0-9]{2}\)? [0-9]{3}-[0-9]{2}-[0-9]{2}$`)
+	if r.MatchString(phone) {
+		return true
+	} else if s.MatchString(phone) {
+		return true
+	}
+
+	return false
 }
