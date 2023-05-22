bbb778d14206e42405aaf07b3b9caa8d0ca76520
sjurabekov1 <sjurabekov1@gmail.com>
2023-05-22 18:39:07 +0500

    add to router

--- api/api.go
+++ api/api.go
@@ -163,7 +163,8 @@ func SetUpRouter(h handlers.Handler, cfg config.Config) (r *gin.Engine) {
 		v2.POST("/login", h.V2Login) // @TODO
 		v2.PUT("/refresh", h.V2RefreshToken)
 		v2.PUT("/refresh-superadmin", h.V2RefreshTokenSuperAdmin)
-		v2.POST("/login/superadmin", h.V2LoginSuperAdmin)      // @TODO
+		v2.POST("/login/superadmin", h.V2LoginSuperAdmin) // @TODO
+		v2.POST("/login/with-option", h.V2LoginWithOption)
 		v2.POST("/multi-company/login", h.V2MultiCompanyLogin) // @TODO
 		v2.POST("/multi-company/one-login", h.V2MultiCompanyOneLogin)
 		v2.POST("/user/invite", h.AddUserToProject)
