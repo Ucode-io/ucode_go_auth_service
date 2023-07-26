6c7281c5b12835ec1e46bdc369bc811ba5b93cde
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-07-26 18:19:10 +0500

    remove global permission api

--- api/api.go
+++ api/api.go
@@ -154,7 +154,6 @@ func SetUpRouter(h handlers.Handler, cfg config.Config) (r *gin.Engine) {
 
 		v2.GET("/role-permission/detailed/:project-id/:role-id", h.GetListWithRoleAppTablePermissions)
 		v2.PUT("/role-permission/detailed", h.UpdateRoleAppTablePermissions)
-		v2.GET("/role-golabal-permission/:project-id/:role-id", h.GetGlobalPermission)
 
 		v2.GET("/menu-permission/detailed/:project-id/:role-id/:parent-id", h.GetListMenuPermissions)
 		v2.PUT("/menu-permission/detailed", h.UpdateMenuPermissions)
