c5065096d985e20326c36cac93f6d7341549f8ef
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-08-01 16:31:33 +0500

    fix has access in has acces user

--- grpc/service/session_service_v2.go
+++ grpc/service/session_service_v2.go
@@ -1542,9 +1542,15 @@ func (s *sessionService) V2HasAccessUser(ctx context.Context, req *pb.V2HasAcces
 		s.log.Error("---V2HasAccessUser--->AccessDenied--->", logger.Error(err))
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
-
+	var checkPermission bool
 	// guess role check
-	if session.RoleId != "027944d2-0460-11ee-be56-0242ac120002" {
+	for _, path := range arr_path {
+		if path == "object" || path == "object-slim" {
+			checkPermission = true
+			break;
+		}
+	}
+	if session.RoleId != "027944d2-0460-11ee-be56-0242ac120002" && checkPermission {
 		var tableSlug string
 		if arr_path[len(arr_path)-1] == ":object_id" {
 			tableSlug = arr_path[len(arr_path)-2]
