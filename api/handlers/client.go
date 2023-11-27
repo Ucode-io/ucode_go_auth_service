9e74a7edc3817cbd0899a687012edaf204ea40ce
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2023-11-27 14:12:26 +0500

    offset limit static number

--- api/handlers/client.go
+++ api/handlers/client.go
@@ -276,17 +276,13 @@ func (h *Handler) CreateClientType(c *gin.Context) {
 // @Response 400 {object} http.Response{data=string} "Invalid Argument"
 // @Failure 500 {object} http.Response{data=string} "Server Error"
 func (h *Handler) GetClientTypeList(c *gin.Context) {
-	offset, err := h.getOffsetParam(c)
-	if err != nil {
-		h.handleResponse(c, http.InvalidArgument, err.Error())
-		return
-	}
+	offset := 0
+	// if err != nil {
+	// 	h.handleResponse(c, http.InvalidArgument, err.Error())
+	// 	return
+	// }
 
-	limit, err := h.getLimitParam(c)
-	if err != nil {
-		h.handleResponse(c, http.InvalidArgument, err.Error())
-		return
-	}
+	limit := 100
 
 	resp, err := h.services.ClientService().GetClientTypeList(
 		c.Request.Context(),
