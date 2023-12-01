60356721817cc5b8de336126be043a704ea2a47b
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2023-12-01 11:06:50 +0500

    token for 24 hours

--- config/constants.go
+++ config/constants.go
@@ -10,7 +10,7 @@ const (
 	// DatabaseTimeLayout
 	DatabaseTimeLayout string = time.RFC3339
 	// AccessTokenExpiresInTime ... 1 * 24 *
-	AccessTokenExpiresInTime time.Duration = 1 * 1 * 2 * time.Minute
+	AccessTokenExpiresInTime time.Duration = 1 * 1 * 24 * time.Minute
 	// RefreshTokenExpiresInTime ... 30 * 24 * 60
 	RefreshTokenExpiresInTime time.Duration = 30 * 24 * 60 * time.Minute
 
