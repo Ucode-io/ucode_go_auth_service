289b35524ffb0649bcd196346278d05aeb8e6d63
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-12-19 13:43:49 +0500

    restart

--- api/api.go
+++ api/api.go
@@ -178,7 +178,7 @@ func SetUpRouter(h handlers.Handler, cfg config.BaseConfig) (r *gin.Engine) {
 		v2.PUT("/refresh-superadmin", h.V2RefreshTokenSuperAdmin)
 		v2.POST("/login/superadmin", h.V2LoginSuperAdmin)      // @TODO
 		v2.POST("/multi-company/login", h.V2MultiCompanyLogin) // @TODO
-		v2.POST("/multi-company/one-login", h.V2MultiCompUanyOneLogin)
+		v2.POST("/multi-company/one-login", h.V2MultiCompanyOneLogin)
 		v2.POST("/user/invite", h.AddUserToProject)
 		v2.POST("/user/check", h.V2GetUserByLoginType)
 
