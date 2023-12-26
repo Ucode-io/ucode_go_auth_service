75a011e735e09ef9274b49ccf96e8a4059f00a9c
islombekrakhmonov <islombek.rakhmanov00@gmail.com>
2023-12-26 15:07:54 +0500

    auth_ping_service

---
+++ grpc/service/ping.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+	pb "ucode/ucode_go_auth_service/genproto/auth_service"
+
+	l "ucode/ucode_go_auth_service/pkg/logger"
+
+	"github.com/golang/protobuf/ptypes/empty"
+)
+
+// AdminService ...
+type PingService struct {
+	logger l.LoggerI
+	pb.UnimplementedAuthPingServiceServer
+}
+
+func NewPingService(log l.LoggerI, services ServiceNodesI) *PingService {
+	return &PingService{
+		logger: log,
+	}
+}
+
+func (s *PingService) Ping(ctx context.Context, req *empty.Empty) (res *pb.PingResponse, err error) {
+	return &pb.PingResponse{
+		Message: "Pong",
+	}, nil
+}
