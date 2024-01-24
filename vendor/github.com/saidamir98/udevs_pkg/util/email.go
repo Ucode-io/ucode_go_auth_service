169914c820465372efcc926b26339fa06ec18f7c
Asadbek <asadbek30665@gmail.com>
2024-01-24 16:35:54 +0500

    fixed validate email

--- vendor/github.com/saidamir98/udevs_pkg/util/email.go
+++ vendor/github.com/saidamir98/udevs_pkg/util/email.go
@@ -11,6 +11,18 @@ func IsValidEmail(email string) bool {
 	if len(email) > 254 {
 		return false
 	}
-	r := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`)
+	r := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,10}$`)
 	return r.MatchString(email)
+}
+
+func IsValidEmailNew(email string) bool {
+	// Define the regular expression pattern for a valid email address
+	// This is a basic pattern and may not cover all edge cases
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+
+	// Compile the regular expression
+	re := regexp.MustCompile(emailRegex)
+
+	// Use the MatchString method to check if the email matches the pattern
+	return re.MatchString(email)
 }
