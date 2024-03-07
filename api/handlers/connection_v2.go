f82f925827672585563e740b32d101d08911c0db
o.muzropov <otabek.muzropov2001@gmail.com>
2024-03-07 07:51:08 +0500

    connection list filter by client type id is fixed

--- api/handlers/connection_v2.go
+++ api/handlers/connection_v2.go
@@ -280,9 +280,9 @@ func (h *Handler) V2GetConnectionList(c *gin.Context) {
 	}
 
 	structData, err := helper.ConvertMapToStruct(map[string]interface{}{
-		"limit":          limit,
-		"offset":         offset,
-		"client_type_id": c.DefaultQuery("client_type_id", ""),
+		"limit":                     limit,
+		"offset":                    offset,
+		"client_type_id_from_token": c.DefaultQuery("client-type-id", ""),
 	})
 	if err != nil {
 		h.handleResponse(c, http.InvalidArgument, err.Error())
