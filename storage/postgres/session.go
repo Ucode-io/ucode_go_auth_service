7fecc80c5edd60c6e5a781aa578b37abcb87c43e
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-03-28 12:12:59 +0500

    session delete

--- storage/postgres/session.go
+++ storage/postgres/session.go
@@ -443,12 +443,7 @@ func (r *sessionRepo) ExpireSessions(ctx context.Context, entity *pb.ExpireSessi
 		quotedSessionIDs[i] = "'" + id + "'"
 	}
 
-	queryInitial := `UPDATE "session" SET
-        expires_at = now(),
-        is_changed = TRUE,
-        updated_at = now()
-		WHERE id IN (` + strings.Join(quotedSessionIDs, ",") + `)
-	`
+	queryInitial := `DELETE FROM "session" WHERE id IN (` + strings.Join(quotedSessionIDs, ",") + `)`
 
 	result, err := r.db.Exec(ctx, queryInitial)
 	if err != nil {
