183aff93d18e3a72928b5570aef3da251242b221
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2024-11-25 09:09:25 +0500

    add client platform id to api keys

--- storage/postgres/api_keys.go
+++ storage/postgres/api_keys.go
@@ -114,7 +114,9 @@ func (r *apiKeysRepo) GetList(ctx context.Context, req *pb.GetListReq) (*pb.GetL
 				client_type_id,
 				rps_limit,
 				monthly_request_limit,
-				(SELECT request_count FROM api_key_usage WHERE api_key=app_id AND creation_month=TO_CHAR(DATE_TRUNC('month', CURRENT_TIMESTAMP), 'YYYY-MM-DD')::DATE)
+				(SELECT request_count FROM api_key_usage WHERE api_key=app_id AND creation_month=TO_CHAR(DATE_TRUNC('month', CURRENT_TIMESTAMP), 'YYYY-MM-DD')::DATE),
+				client_platform_id,
+				disable
 			FROM
 			    api_keys`
 
@@ -198,6 +200,8 @@ func (r *apiKeysRepo) GetList(ctx context.Context, req *pb.GetListReq) (*pb.GetL
 			&row.RpsLimit,
 			&row.MonthlyRequestLimit,
 			&usedCount,
+			&row.ClientPlatformId,
+			&row.Disable,
 		)
 
 		if err != nil {
