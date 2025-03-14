05a17e0547a867382c0dba331ae13c7a99c23991
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2025-03-14 09:11:15 +0500

    rollback token expire time

--- config/constants.go
+++ config/constants.go
@@ -9,13 +9,13 @@ import (
 
 const (
 	// Default Configs
-	DatabaseQueryTimeLayout string = `'YYYY-MM-DD"T"HH24:MI:SS"."MS"Z"TZ'`
-	DatabaseTimeLayout      string = time.RFC3339
-	// AccessTokenExpiresInTime  time.Duration = 1 * 60 * 24 * time.Minute
-	// RefreshTokenExpiresInTime time.Duration = 30 * 24 * 60 * time.Minute
+	DatabaseQueryTimeLayout   string        = `'YYYY-MM-DD"T"HH24:MI:SS"."MS"Z"TZ'`
+	DatabaseTimeLayout        string        = time.RFC3339
+	AccessTokenExpiresInTime  time.Duration = 1 * 60 * 24 * time.Minute
+	RefreshTokenExpiresInTime time.Duration = 30 * 24 * 60 * time.Minute
 
-	AccessTokenExpiresInTime  time.Duration = 90 * time.Second
-	RefreshTokenExpiresInTime time.Duration = 150 * time.Second
+	// AccessTokenExpiresInTime  time.Duration = 90 * time.Second
+	// RefreshTokenExpiresInTime time.Duration = 150 * time.Second
 
 	ProjectID             string = "f5955c82-f264-4655-aeb4-86fd1c642cb6"
 	AdminClientPlatformID string = "7d4a4c38-dd84-4902-b744-0488b80a4c01"
