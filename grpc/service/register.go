2cfe74189fdd5680da1c5991bb4b3558903b9d2d
webslingerr <shokhrukhsafarov2003@gmail.com>
2025-07-02 16:46:34 +0500

    delete from user project where error from object builder

--- grpc/service/register.go
+++ grpc/service/register.go
@@ -226,6 +226,14 @@ func (rs *registerService) RegisterUser(ctx context.Context, data *pb.RegisterUs
 			ProjectId: data.ResourceEnvironmentId,
 		})
 		if err != nil {
+			_, _ = rs.strg.User().DeleteUserFromProject(ctx, &pb.DeleteSyncUserRequest{
+				UserId:        userId,
+				RoleId:        data.RoleId,
+				CompanyId:     data.CompanyId,
+				ProjectId:     data.ProjectId,
+				ClientTypeId:  data.ClientTypeId,
+				EnvironmentId: data.EnvironmentId,
+			})
 			rs.log.Error("!!!PostgresObjectBuilderService.CreateUser--->", logger.Error(err))
 			return nil, status.Error(codes.InvalidArgument, err.Error())
 		}
